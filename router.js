2560aab3fb7d3753715dcb159d3445dc4495d192
Roxanne Lopez van Dooren <roxanne.lopez.dooren@gmail.com>
2016-03-09 10:05:50 +0100

    Testing deploy

--- router.js
+++ router.js
@@ -6,6 +6,11 @@ var auth = require('./services/accessSvc');
 
 module.exports = function(app) {
 	var users = express.Router();
+
+	//TEST
+	app.get('/test', function(req, res){
+		res.json({msg:'Funcionando...'});
+	})
 	
 	//USERS
 	app.use('/users', users);
