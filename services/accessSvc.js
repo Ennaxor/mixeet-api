27b66115bbfd7a9af01b92a8aac9ed2d2d558900
Roxanne Lopez van Dooren <roxanne.lopez.dooren@gmail.com>
2016-03-16 17:30:04 +0100

    fixed possible authorize function

--- services/accessSvc.js
+++ services/accessSvc.js
@@ -1,33 +1,50 @@
 var User = require('../models/user');
 
 module.exports = {
-	authorize: function(req, res, next){
-		var u_model = User;
-		var bearerHeader = req.headers["authorization"];
-		if(bearerHeader){
-			var bearer = bearerHeader.split(" ");
-			var type = bearer[0];
-			var token = bearer[1];
-
+	authorize: function(){
+		return function(req, res, next){
+			var u_model = User.model;
+			var bearerHeader = req.headers["authorization"];
+			var token = "";
+			if(req.params.access_token){
+				token = req.params.access_token;
+			}
+			else if(bearerHeader){
+					var bearer = req.headers["authorization"].split(" ");
+					if(bearer.length > 1) token = bearer[1];
+					else{
+						res.status(400);
+						res.json({message:"Wrong auth data", status: 401});
+						return;	
+					}
+				
+			}
+			else{ //IF NOT TOKEN OR SHIT
+				if(options.strict){
+					res.status(400);
+					res.json({message:"Auth data missing", status: 400});
+					return;
+				}
+	        	else{
+	        		return next();	
+	        	}
+			}
+			//IF TOKEN SET
 			u_model.findOne({token:token})
-					.exec(function(err, user){
-						if (err) res.status(500).json({message:"MongoDB Error"});
-						else{
-							if(user){
-								req.user = user;
-								if(user.token_expiry_date > (new Date())) next();
-								else res.status(404).json({message:"Token expired"});
-							}
-							else{
-								res.status(401).json({message:"Wrong Auth Data"});
-							}
-						}
-					})
+			.then(function(result){
+				req.user = result;
+				if(user.token_expiry_date > (new Date())) next();
+				else res.status(404).json({message:"Token expired"});
+				return next();
+			})
+			.catch(function(err){
+				console.log(err);
+				res.status(403);
+				res.json({message:"Unauthorized", status: 403});	
+			})
+
 		}
-		else{
-			res.status(400).json({message:"Auth Data Missing"});
-		}
-	}
+	}	
 
 
 }
