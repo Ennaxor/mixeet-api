5a8f95c8fe4f5443d094efb1f2426986595be73c
Roxanne Lopez van Dooren <roxanne.lopez.dooren@gmail.com>
2016-03-16 15:10:37 +0100

    no message

--- server.js
+++ server.js
@@ -9,10 +9,21 @@ var configDB = require('./config/database.js');
 var mongoose = require('mongoose');
 mongoose.connect(process.env.DB);
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 app.use(require('cookie-parser')());
 app.use(require('body-parser').json());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(allowCrossDomain);
+
 
 /*app.use(function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
